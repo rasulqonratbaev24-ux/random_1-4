a3f5e8dae61708341f6c03c9767c1b6f381b4fae
rasulqonratbaev24-ux <rasulqonratbaev24@gmail.com>
2026-04-21 20:27:53 +0500

    Update server.js

--- server.js
+++ server.js
@@ -8,17 +8,20 @@ let numbers = [
   4,4,4,4,4,4
 ];
 
-// random aralashtirish
 numbers.sort(() => Math.random() - 0.5);
 
 // API
 app.get("/get-number", (req, res) => {
   let num = numbers.pop();
-  if (!num) return res.send("Tugadi");
-  res.send(num.toString());
+  if (!num) return res.json({ num: "Tugadi", left: 0 });
+
+  res.json({
+    num: num,
+    left: numbers.length
+  });
 });
 
-// FRONTEND (asosiy sahifa)
+// FRONTEND
 app.get("/", (req, res) => {
   res.send(`
     <!DOCTYPE html>
@@ -29,6 +32,7 @@ app.get("/", (req, res) => {
     <body style="display:flex;flex-direction:column;justify-content:center;align-items:center;height:100vh;font-family:sans-serif;">
       
       <h1 id="number" style="font-size:120px;">?</h1>
+      <p id="left" style="font-size:20px;">Qolgan: 24</p>
       
       <button onclick="getNumber()" style="font-size:30px;padding:10px 20px;cursor:pointer;">
         Random olish
@@ -37,8 +41,10 @@ app.get("/", (req, res) => {
       <script>
         async function getNumber() {
           let res = await fetch('/get-number');
-          let data = await res.text();
-          document.getElementById('number').innerText = data;
+          let data = await res.json();
+
+          document.getElementById('number').innerText = data.num;
+          document.getElementById('left').innerText = "Qolgan: " + data.left;
         }
       </script>
 
